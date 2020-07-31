f4aaed61c5ed95fb95090a65b019e0b9d650602c
rawaldeep <rawaldeepsingh26@gmail.com>
2020-07-31 17:10:34 +0200

    unfinished isotope

--- src/components/PortfolioArea.js
+++ src/components/PortfolioArea.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import RSC from "react-scrollbars-custom";
-import IsoTopeGrid from "react-isotope";
+import Isotope from "isotope-layout";
 import { TechLogos } from './TechLogos';
 import portfolio1 from '../assets/images/revamp.jpeg';
 import portfolio2 from '../assets/images/groversgsm.png';
@@ -11,40 +11,68 @@ import portfolio6 from '../assets/images/omegafinancial.jpeg';
 import portfolio7 from '../assets/images/UwBouwdroger.jpeg';
 import portfolio8 from '../assets/images/VIAImmobilien.jpeg'
 import portfolio9 from '../assets/images/FantastischBelgisch.jpeg';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const filtersDefault = [
    { id: 1, label: "All", isChecked: true },
    { id: 2, label: "Wordpress", isChecked: false },
-   { id: 3, label: "Front End", isChecked: false },
-   { id: 4, label: "UI UX Design", isChecked: false }
+   { id: 3, label: "Front-End", isChecked: false },
+   { id: 4, label: "UI-UX-Design", isChecked: false }
  ];
 
 const grid = [
-   {item: "Sass Landing", image: portfolio1, type: "Front End"},
+   {item: "Sass Landing", image: portfolio1, type: "Front-End"},
    {item: "ECommerce", image: portfolio2, type: "Wordpress"},
-   {item: "Photografer Portfolio", image: portfolio3, type: "Front End"},
+   {item: "Photografer Portfolio", image: portfolio3, type: "Front-End"},
    {item: "404", image: portfolio4, type: "UI UX Design"},
    {item: "Prizm", image: portfolio5, type: "Wordpress"},
    {item: "Omega financial website", image: portfolio6, type: "Wordpress"},
    {item: "Uw Bouwdroger", image: portfolio7, type: "Wordpress"},
-   {item: "VIA Immobilien", image: portfolio8, type: "UI UX Design"},
+   {item: "VIA Immobilien", image: portfolio8, type: "UI-UX-Design"},
    {item: "FantastischBelgisch", image: portfolio9, type: "Wordpress"}
 ]
 
 export const PortfolioArea = ({menu}) => {
    const [filters, setFilters] = useState(filtersDefault);
+   const [isotope, setIsotope]  = useState(null);
+   const [filterKey, setFilterKey] = useState("All");
+   const intializeIsotope = () =>{
+      setIsotope(
+         new Isotope(".pt-portfolio-items", {
+           itemSelector: ".pt-portfolio-item",
+           layoutMode: "fitRows"
+         })
+       );
+   }
+   useEffect(intializeIsotope, []);
+    useEffect(
+      () => {
+        if (isotope) {
+          filterKey === "All"
+            ? isotope.arrange({ filter: `*` })
+          : isotope.arrange({ filter: `.${filterKey}` });
+        }
+      },
+      [isotope, filterKey]
+    );
+    useEffect(()=>{
+      setFilterKey('All')
+    }, []);
    const handleFilterclick = (id) =>{
       let arr = []; 
       filtersDefault.forEach(element => {
          element.isChecked = false
          if(element.id === id){
             element.isChecked = true
+         
+            setFilterKey(element.label)
+            
          }
          arr.push(element)
       });
       setFilters(arr);
    }
+
    
     return (
         <section className={menu ? "area-inner area-started active": "area-inner area-started active paddingAdjust"} >
